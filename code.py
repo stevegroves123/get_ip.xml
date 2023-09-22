cf23852d319682f355c67421eac107e4be3a5bfe
Steve Groves <stevegroves123@gmail.com>
2023-09-22 07:38:06 +0100

    Testcode.py

--- code.py
+++ code.py
@@ -53,8 +53,8 @@ display.show()
 while True:
     # look for file - if not found show error
     try:
-        fp = open("get_ip.xml", "r") #change r for w and see if it overwrites
-        data = fp.read()
+        fp = open("get_ip.xml", "r")
+        data = fp.read() #this holds all the text
         try:
             startGenState = data.index("disconnected")
         except ValueError:
